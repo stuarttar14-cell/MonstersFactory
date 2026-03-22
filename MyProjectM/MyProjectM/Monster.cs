249947ea802b2d0f0c43ea184fa50f4a27892fad
Mati <141156428+DrLatee@users.noreply.github.com>
2026-03-22 13:22:22 +0200

    Add files via upload

---
+++ MyProjectM/MyProjectM/Monster.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectM
+{
+    internal class MyMonster
+    {
+        private string name;
+        private int power;
+        private int health;
+
+        public MyMonster(string name, int power, int health)
+        {
+            this.name = name;
+            this.power = power;
+            this.health = health;
+        }
+
+        public string GetName() { return name; }
+        public int GetPower() { return power; }
+        public int GetHealth() { return health; }
+        public bool IsAlive() { return health > 0; }
+        public int Attack() { return power; }
+
+        public void TakeDamage(int damage)
+        {
+            health = health - damage;
+            if (health < 0)
+            {
+                health = 0;
+            }
+        }
+
+        public void Heal(int points)
+        {
+            health = health + points;
+        }
+
+        public void PowerUp(int extraPower)
+        {
+            power = power + extraPower;
+        }
+
+        public void Roar()
+        {
+            Console.WriteLine(name + " ROARS!!!");
+            int[] sounds = { 300, 360, 420, 500, 420, 360, 280 };
+            int[] times = { 120, 120, 150, 220, 150, 120, 250 };
+
+            for (int i = 0; i < sounds.Length; i++)
+            {
+                Console.Beep(sounds[i], times[i]);
+            }
+        }
+
+        public void ShowMonster()
+        {
+            Console.WriteLine("############################");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Power: " + power);
+            Console.WriteLine("Health: " + health);
+
+            if (IsAlive())
+                Console.WriteLine("Status: Alive");
+            else
+                Console.WriteLine("Status: Dead");
+
+            Console.WriteLine("############################");
+        }
+    }
+}
---
+++ MyProjectM/MyProjectM/Program.cs
@@ -0,0 +1,67 @@
+﻿using MyProjectM;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void F()
+    {
+        Console.WriteLine("Mati");
+    }
+    public static void Main()
+    {
+        F();
+        Console.WriteLine("How many monsters do you want");
+        int monstersAmount=int.Parse(Console.ReadLine());
+        MyMonster[] monsters = new MyMonster[monstersAmount];
+        for (int i = 0; i < monstersAmount; i++)
+        {
+            Console.WriteLine("Enter name");
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter power");
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter health");
+            int health = int.Parse(Console.ReadLine());
+            monsters[i] = new MyMonster(name, power, health);
+        }
+        for (int i = 0; i < monstersAmount; i++)
+        {
+            monsters[i].ShowMonster();
+        }
+        int monstersAlive=0;
+        for (int i = 0; i < monstersAmount; i++)
+        {
+            if(monsters[i].IsAlive())
+            {  
+                monstersAlive++; 
+            }
+        }
+        Console.WriteLine(monstersAlive);
+        int maxPower = 0;
+        for (int i = 1; i < monstersAmount; i++)
+        {
+            if (monsters[i].GetPower() > monsters[i-1].GetPower())
+            {
+                maxPower = i;
+            }
+        }
+        Console.WriteLine(monsters[maxPower].GetName() + " is the strongest with the power of "+ monsters[maxPower].GetPower());
+        for (int i = 0;i < monstersAmount; i++)
+        {
+            Console.WriteLine("Enter damage");
+            int damage= int.Parse(Console.ReadLine());
+            monsters[i].TakeDamage(damage);
+        }
+        for (int i = 0; i < monstersAmount; i++)
+        {
+            monsters[i].ShowMonster();
+        }
+        for (int i = 0; i < monstersAmount; i++)
+        {
+            if (monsters[i].GetHealth()<60)
+            {
+                Console.WriteLine(monsters[i].GetName()); 
+            }
+        }
+    }
+}
